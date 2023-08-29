67befa602a2500583610a58c0781cd110fd4155d
renaraff <refurtado2007@gmail.com>
2023-08-29 16:40:32 -0300

    Funcionando

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
+
 function App() {
+
+  const [ listaTarefas, setListaTarefas ] = useState ([]);
+  const [tarefa, setTarefa ] = useState ( {id: '' , texto: ''});
+
+  function addTarefa()
+  {
+    if( tarefa.texto !=="" ){
+      setListaTarefas([...listaTarefas, tarefa ]);
+    }
+  }
+  useEffect(() =>{
+      setTarefa( { id: "", texto: "" });
+  }, [listaTarefas])
+
+  function excluirTarefa(id) {
+    const novalista = listaTarefas.filter( tarefa => tarefa.id !== id);
+    setListaTarefas(novalista);
+  }
+
+
   return (
-   <>
-   </>
+    <>
+    <header>
+      <h1>TO DO LIST</h1>
+    </header>
+      <div>
+        <input type="text" nome= "tarefa" placeholder="Digite sua tarefa" value={tarefa.texto} onChange={ (e) => setTarefa( {id: Math.random(), texto: e.target.value })} />
+        <button onClick={addTarefa}>Add itens</button>
+    
+      </div>
+      <div>
+        <ul> 
+          {listaTarefas.map( (item, index ) => (
+           <li key={index}>{item.texto} <button onClick={() => excluirTarefa(item.id) }>Excluir</button></li>
+          ))}
+        </ul>
+      </div>
+    </>
+   
   );
 }
 
